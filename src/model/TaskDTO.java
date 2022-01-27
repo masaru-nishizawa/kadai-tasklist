0583711c2e1a5608e433efed6344a6c0a6ef51f4
masaru.nishizawa <mnishizawa2016hg@gmail.com>
2022-01-27 21:35:01 +0900

    DTOの初期状態

---
+++ src/model/TaskDTO.java
@@ -0,0 +1,57 @@
+package model;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tasks")
+public class TaskDTO {
+    @Id
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "time_created")
+    private Timestamp time_created;
+
+    @Column(name = "time_updated")
+    private Timestamp time_updated;
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Timestamp getTime_created() {
+        return time_created;
+    }
+
+    public void setTime_created(Timestamp time_created) {
+        this.time_created = time_created;
+    }
+
+    public Timestamp getTime_updated() {
+        return time_updated;
+    }
+
+    public void setTime_updated(Timestamp time_updated) {
+        this.time_updated = time_updated;
+    }
+}
